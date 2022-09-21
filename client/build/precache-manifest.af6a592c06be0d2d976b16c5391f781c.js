d31678204793764ac258702e8bc32faf95198def
Chaitanya Rai <Chaitanyarai899@gmail.com>
2022-09-22 00:36:10 +0530

    initial commit

---
+++ client/build/precache-manifest.af6a592c06be0d2d976b16c5391f781c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fb9268008bc7f4ca045816b003f6a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3f133c92b575d4f3fc4",
+    "url": "/static/css/main.e3b7569c.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f133c92b575d4f3fc4",
+    "url": "/static/js/main.585d3250.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "b365d0822024336c51d3dbbb7125f4bf",
+    "url": "/static/media/bgcoffee.b365d082.jpg"
+  },
+  {
+    "revision": "5de75e1b32b8350c1c875dbbcb732393",
+    "url": "/static/media/logo.5de75e1b.png"
+  }
+]);
