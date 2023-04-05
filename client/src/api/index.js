7d8d66808ff0b3a0a693a7241ee0b4e5ca8e8070
Chaitanya Rai <95732261+Chaitanyarai899@users.noreply.github.com>
2023-04-06 01:34:27 +0530

    Server migrated from heroku to render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://cafereviews.herokuapp.com/posts';
+const url = 'https://yourcafes.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
